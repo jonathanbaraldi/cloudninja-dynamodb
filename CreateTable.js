aad12bb1000eb277fc0287c22d90207a56afe77c
Jonathan Dias Baraldi <jonathanbaraldi@gmail.com>
2016-02-22 19:55:10 -0300

    Primeiro commit

---
+++ CreateTable.js
@@ -0,0 +1,137 @@
+var AWS = require("aws-sdk");
+    
+AWS.config.update(
+                    {
+                      region:'us-east-1'
+                    }
+);
+
+    
+var dynamodb = new AWS.DynamoDB();  
+
+    var ProductCatalog = {
+      TableName: 'ProductCatalog',
+      KeySchema: [ 
+          { 
+              AttributeName: 'Id', KeyType: 'HASH'
+          }
+      ],
+      AttributeDefinitions: [ 
+          {   AttributeName: 'Id', AttributeType: 'N' }
+      ],
+      ProvisionedThroughput: { 
+          ReadCapacityUnits: 1, 
+          WriteCapacityUnits: 1, 
+      }
+  };
+  dynamodb.createTable(ProductCatalog, function(err, data) {
+      if (err) {
+        console.log("ERROR");
+        console.log(err);
+      } else { 
+        console.log("OK");
+        console.log(data);
+      }
+
+  });
+
+
+  var Forum = {
+      TableName: 'Forum',
+      KeySchema: [ 
+          { 
+              AttributeName: 'Name', KeyType: 'HASH'
+          }
+      ],
+      AttributeDefinitions: [ 
+          {   AttributeName: 'Name', AttributeType: 'S' }
+      ],
+      ProvisionedThroughput: { 
+          ReadCapacityUnits: 1, 
+          WriteCapacityUnits: 1, 
+      }
+  };
+  dynamodb.createTable(Forum, function(err, data) {
+      if (err) {
+        console.log("ERROR");
+        console.log(err);
+      } else { 
+        console.log("OK");
+        console.log(data);
+      }
+
+  });
+
+
+  var Thread = {
+      TableName: 'Thread',
+      KeySchema: [ 
+          { 
+              AttributeName: 'ForumName', KeyType: 'HASH'
+          },
+          { 
+              AttributeName: 'Subject', KeyType: 'RANGE', 
+          }
+      ],
+      AttributeDefinitions: [ 
+          {   AttributeName: 'ForumName', AttributeType: 'S' },
+          {   AttributeName: 'Subject', AttributeType: 'S' },
+      ],
+      ProvisionedThroughput: { 
+          ReadCapacityUnits: 1, 
+          WriteCapacityUnits: 1, 
+      }
+  };
+  dynamodb.createTable(Thread, function(err, data) {
+      if (err) {
+        console.log("ERROR");
+        console.log(err);
+      } else { 
+        console.log("OK");
+        console.log(data);
+      }
+  });
+
+
+  var Reply = {
+      TableName: 'Reply',
+      KeySchema: [ 
+          { 
+              AttributeName: 'Id', KeyType: 'HASH'
+          },
+          { 
+              AttributeName: 'ReplyDateTime', KeyType: 'RANGE', 
+          }
+      ],
+      AttributeDefinitions: [ 
+          {   AttributeName: 'Id', AttributeType: 'S' },
+          {   AttributeName: 'ReplyDateTime', AttributeType: 'S' },
+          {   AttributeName: 'PostedBy', AttributeType: 'S' }
+
+      ],
+      ProvisionedThroughput: { 
+          ReadCapacityUnits: 1, 
+          WriteCapacityUnits: 1, 
+      },
+      LocalSecondaryIndexes: [ 
+          { 
+              IndexName: 'PostedBy-Message-Index', 
+              KeySchema: [
+                  {   AttributeName: 'Id', KeyType: 'HASH' },
+                  {   AttributeName: 'PostedBy', KeyType: 'RANGE' }
+              ],
+              Projection: { 
+                  ProjectionType: 'KEYS_ONLY' 
+              }
+          }
+      ]
+  };
+  dynamodb.createTable(Reply, function(err, data) {
+      if (err) {
+        console.log("ERROR");
+        console.log(err);
+      } else { 
+        console.log("OK");
+        console.log(data);
+      }
+  });
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # cloudninja-dynamodb
 Repo criado para ser usado no curso preparatório para AWS Certified Developer Associate
+
+
+	sudo su
+	yum update -y
+	yum install git -y
+	yum install nodejs npm —-enablerepo=epel
+	git clone https://github.com/jonathanbaraldi/cloudninja-dynamodb.git
+	cd cloudninja-dynamodb
+	
+	node CreateTable.js
+
+
+===========================
+2 - Carregar as tabelas
+	
+	Rodar na linha de comando.
+
+
+	aws dynamodb batch-write-item --request-items file://ProductCatalog.json
+
+	aws dynamodb batch-write-item --request-items file://Forum.json
+
+	aws dynamodb batch-write-item --request-items file://Thread.json
+
+	aws dynamodb batch-write-item --request-items file://Reply.json
